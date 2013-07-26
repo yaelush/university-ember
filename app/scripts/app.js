b26e42b2ca6c69d2e532bbae165948d4ddcc801f
Yael <yael@yariv-HP-Pavilion-dv6500-Notebook-PC.(none)>
2013-07-26 16:44:23 +0300

    add nerdeez

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,5 +6,11 @@ require('scripts/controllers/*');
 require('scripts/models/*');
 require('scripts/views/*');
 require('scripts/router');
-require('scripts/store');
 
+
+//store
+require('scripts/nerdeez-ember/tastypie_serializer');
+require('scripts/nerdeez-ember/tastypie_adapter');
+require('scripts/nerdeez-ember/porthole.min');
+require('scripts/nerdeez-ember/wormhole');
+require('scripts/store');
